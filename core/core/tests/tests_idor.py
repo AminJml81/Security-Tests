e907f46c8ca10347ddc66e2671f75fae66eaff49
Amin <Aminbest1381@gmail.com>
2025-02-03 23:36:11 +0330

    clean

--- core/core/tests/tests_idor.py
+++ core/core/tests/tests_idor.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.contrib.auth.models import User  # If you have user authentication
 from django.contrib.auth import get_user_model
 
 from core.models import Comment
